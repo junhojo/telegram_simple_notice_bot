ba7f08aea3c24e5655ada1f12854dbe65341e755
jhjo <jhjo@daou.co.kr>
2018-08-09 11:57:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# telegram_simple_notice_bot
---
+++ telegram_simple_notice_bot.py
@@ -0,0 +1,22 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from telegram.ext import Updater, CommandHandler
+import sched
+
+#pip install apscheduler
+#http://apscheduler.readthedocs.io/en/3.0/modules/triggers/cron.html#module-apscheduler.triggers.cron
+
+def notiMessage(bot, update):
+    update.message.reply_text('[공지] 주간업무를 공유해 주세요')
+
+def start(bot, update):
+    # sched.add_job(notiMessage, 'cron', second='*/5', id="test_1", args=[bot, update])
+    #sched.add_job(notiMessage, 'cron', second='0 30 13 ? * WED *', id="test_1", args=[bot, update])
+    sched.add_job(notiMessage, 'cron', day_of_week='wed', hour="13", minute='03', second='00', id="notice", args=[bot, update])
+
+sched = BackgroundScheduler()
+sched.start()
+updater = Updater('691829804:AAH_JzAtsxvFckHlvFmwKSjhDOV-6o7jnf8')
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.start_polling()
+updater.idle()
+
